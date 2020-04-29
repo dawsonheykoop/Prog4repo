84910efb15a5a7d2bf6c7455bf7505f507453ddc
dawsonheykoop <dawsonheykoop@192.168.0.235>
2020-04-28 18:50:20 -0700

    Setup and connection to oracle is successful as far as I can tell

--- Prog4/src/Controller.java
+++ Prog4/src/Controller.java
@@ -1,19 +1,68 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 // A controller to interact with the view and oracle
 public class Controller {
 	
+	private Connection dbconn;	// Connection to JDBC
+	private String username;	// an authorized user to connect to the database
+	private String password;	// the password the authorized account
+	
 	//TODO make the constructor for the view
-	public Controller() {
-		
+	public Controller(String username, String password) {
+		this.username = username;
+		this.password = password;
+		this.setup(username,password);
 	}
 	
 	//TODO
 	/**
 	 * setup()
 	 * 
-	 * Connects the program to oracle
+	 * Connects the program to oracle using the given username and password and
+	 * initializes the connection variable object.
 	 */
-	public void setup() {
-		
+	private void setup(String username, String password) {
+		final String oracleURL =   // Magic lectura -> aloe access spell
+                "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
+	        
+	     // load the (Oracle) JDBC driver by initializing its base
+         // class, 'oracle.jdbc.OracleDriver'.
+
+        try {
+
+                Class.forName("oracle.jdbc.OracleDriver");
+
+        } catch (ClassNotFoundException e) {
+
+                System.err.println("*** ClassNotFoundException:  "
+                    + "Error loading Oracle JDBC driver.  \n"
+                    + "\tPerhaps the driver is not on the Classpath?");
+                System.exit(-1);
+
+        }
+        
+        // make and return a database connection to the user's
+        // Oracle database
+
+	    Connection dbconn = null;
+	
+	    try {
+	            dbconn = DriverManager.getConnection
+	                           (oracleURL,username,password);
+	
+	    } catch (SQLException e) {
+	
+	            System.err.println("*** SQLException:  "
+	                + "Could not open JDBC connection.");
+	            System.err.println("\tMessage:   " + e.getMessage());
+	            System.err.println("\tSQLState:  " + e.getSQLState());
+	            System.err.println("\tErrorCode: " + e.getErrorCode());
+	            System.exit(-1);
+	
+	    }
+	    this.dbconn = dbconn;
 	}
 	
 	//TODO
--- Prog4/src/Prog4.java
+++ Prog4/src/Prog4.java
@@ -1,3 +1,4 @@
+import java.sql.Connection;
 
 public class Prog4 {
 	
@@ -5,7 +6,27 @@ public class Prog4 {
 	// java Prog4 <oracle username> <oracle password>
 	// 
 	public static void main(String[] args) {
-		
+		String username = null,    // Oracle DBMS username
+	           password = null;    // Oracle DBMS password
+
+        if (args.length == 2) {    // get username/password from cmd line args
+            username = args[0];
+            password = args[1];
+        } else {
+            System.out.println("\nUsage:  java Prog4 <username> <password>\n"
+                             + "    where <username> is your Oracle DBMS"
+                             + " username,\n    and <password> is your Oracle"
+                             + " password (not your system password).\n");
+            System.exit(-1);
+        }
+        
+        // Create the controller with the authorized user's credentials.
+        
+        Controller prog4_controller = new Controller(username,password);
+       
+        System.out.println("Successfully connected to the database.\n");
+	        
+	        
 	}
 
 }
